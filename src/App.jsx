fb81e9ab6b7588af1ea28b8bc8c306bb67adcb9e
g g <gabedotgutierrez@gmail.com>
2024-11-16 11:36:43 -0500

    Stubbed all components, very very basic css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from "./components/navbar/NavBar.jsx"
+import Footer from './components/footer/Footer.jsx'
+import Home from './components/home/Home.jsx'
+import About from './components/about/About.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // TODO: user state for authnetication: also add to context at some point
+  const [user, setUser] = useState(null);
+
+  // TODO: useeffect to load user on signup.
+
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     const user = await verifyUser();
+  //     user ? setUser(user) : setUser(null);
+  //   };
+
+  //   fetchUser();
+  // }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Navbar/>
+        <h1>Hello world!</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      <Footer/>
+    </>  
   )
 }
 
---
+++ src/components/floorView/PantryCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PantryCard() {
+  return (
+    <div>PantryCard</div>
+  )
+}
+
+export default PantryCard
