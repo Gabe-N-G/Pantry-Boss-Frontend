e83ee0613e64f24a4debdb93a2bbe1afcdd35d4d
g g <gabedotgutierrez@gmail.com>
2024-11-20 13:38:54 -0500

    Single Dashboard view/edit/delete made

--- src/components/dashboard/DashboardDispenserCard.jsx
+++ src/components/dashboard/DashboardDispenserCard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getPantryDispensers } from "../../services/dispensers.js";
+import { Link } from "react-router-dom";
 
 const DashboardDispenserCard = (props) => {
     const pantryId = props.pantryId;
@@ -22,33 +23,34 @@ const DashboardDispenserCard = (props) => {
     return (
         <div className="dispenser-container">
             {dispensers.map((dispenser) => (
-                <div
-                    key={dispenser.id}
-                    className={`dispenser-card ${
-                        dispenser.current_level < dispenser.threshold ? "low-stock" : "full-stock"
-                    }`}
-                >
-                    {/* Icon */}
-                    <div className="icon">
-                        {dispenser.current_level < dispenser.threshold ? "⚠️" : "✅"}
+                <Link to={`/dispensers/${dispenser.id}` }>
+                    <div
+                        key={dispenser.id}
+                        className={`dispenser-card ${
+                            dispenser.current_level < dispenser.threshold ? "low-stock" : "full-stock"
+                        }`}
+                    >
+                        {/* Icon */}
+                        <div className="icon">
+                            {dispenser.current_level < dispenser.threshold ? "⚠️" : "✅"}
+                        </div>
+
+                        {/* Type */}
+                        <p className="type"> {friendlyType[dispenser.type]}</p>
+
+                        {/* Stock Level */}
+                        <p className="level">
+                            Level: {dispenser.current_level}/{dispenser.max_capacity}
+                        </p>
+
+                        {/* Alert Message */}
+                        {dispenser.current_level < dispenser.threshold ? (
+                            <p className="alert">Low Stock</p>
+                        ) : (
+                            <p className="alert">Full Stock</p>
+                        )}
                     </div>
-
-                    {/* Type */}
-                    <p className="type"> {friendlyType[dispenser.type]}</p>
-
-                    {/* Stock Level */}
-                    <p className="level">
-                        Level: {dispenser.current_level}/{dispenser.max_capacity}
-                    </p>
-
-                    {/* Alert Message */}
-                    {dispenser.current_level < dispenser.threshold ? (
-                        <p className="alert">Low Stock</p>
-                    ) : (
-                        <p className="alert">Full Stock</p>
-                    )}
-                </div>
-
+                </Link>    
             ))}
         </div>
     );
